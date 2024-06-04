a1910dfc5a58949614cbdbb9509472f651e3510d
999Ralex999 <140698368+999Ralex999@users.noreply.github.com>
2024-06-04 13:24:52 +0300

    2_part_profile_done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import user from '../data/user.json';
+
+import { Profile } from './profile/profile';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
+        marginTop: 20,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        flexDirection: 'column',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
